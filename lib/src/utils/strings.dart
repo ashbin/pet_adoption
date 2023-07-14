3d2eeb3b89dbba4bbcf1629c888ee0cc5b5508d6
ashbin <ashbinmathew22@gmail.com>
2023-07-14 16:00:05 +0530

    Update strings.dart

--- lib/src/utils/strings.dart
+++ lib/src/utils/strings.dart
@@ -3,7 +3,12 @@ class Strings{
   static const searchHint = 'Search with pet name, breed, or species';
 
   static const petDetails = 'Pet Details';
-  static const adopt = 'Adopt';
+  static const adopt = 'Adopt Me';
+  static const okay = 'Okay';
+  static const alreadyAdopted = 'Already Adopted';
+
+  static const success = 'Success';
+  static const adoptedNow = 'You’ve now adopted ';
 
   static const adoptionHistory = 'Adoption History';
   static const adoptionHistoryEmpty = "You haven't adopted any pet.. Go ahead and adopt today";
