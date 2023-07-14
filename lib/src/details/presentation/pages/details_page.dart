a63d462da2bf506e15f5e877718c288b51510281
ashbin <ashbinmathew22@gmail.com>
2023-07-14 15:17:59 +0530

    Handle back press from details page

--- lib/src/details/presentation/pages/details_page.dart
+++ lib/src/details/presentation/pages/details_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pet_adoption/src/details/presentation/bloc/details_bloc.dart';
 import 'package:pet_adoption/src/details/presentation/pages/photo_view.dart';
 import 'package:pet_adoption/src/listing/domain/entities/listing_entity.dart';
+import 'package:pet_adoption/src/utils/strings.dart';
 
 class PetDetailsPage extends StatefulWidget {
   static const routeName = '/details';
@@ -53,104 +54,110 @@ class _PetDetailsPageState extends State<PetDetailsPage> {
         },
         builder: (context, state) {
           var data = state.data;
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Pet Details'),
-              leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: (){
-                  Navigator.of(context).pop(state is DetailsLoadCompleteState);
-              },),
-            ),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Container(
-                    color:
-                        Theme.of(context).canvasColor
-                        ,
-                    child: InkWell(
-                      onTap: () => Navigator.of(context).pushNamed(
-                          AppPhotoView.routeName,
-                          arguments: data.photoUrl),
-                      child: Center(
-                        child: Hero(
-                          tag: data.id,
-                          child: ColorFiltered(
-                            colorFilter: ColorFilter.mode(
-                              data.isAvailable?Colors.transparent : Colors.grey, // or any other color
-                              BlendMode.saturation,
-                            ),
-                            child: Image.network(
-                              data.photoUrl,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Icon(Icons
-                                    .image); // Placeholder icon if image fails to load
-                              },
+          return WillPopScope(
+            onWillPop: () async{
+              Navigator.of(context).pop(state is DetailsLoadCompleteState);
+              return false;
+            },
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text(Strings.petDetails),
+                leading: IconButton(icon: const Icon(Icons.arrow_back), onPressed: (){
+                    Navigator.of(context).pop(state is DetailsLoadCompleteState);
+                },),
+              ),
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                      color:
+                          Theme.of(context).canvasColor
+                          ,
+                      child: InkWell(
+                        onTap: () => Navigator.of(context).pushNamed(
+                            AppPhotoView.routeName,
+                            arguments: data.photoUrl),
+                        child: Center(
+                          child: Hero(
+                            tag: data.id,
+                            child: ColorFiltered(
+                              colorFilter: ColorFilter.mode(
+                                data.isAvailable?Colors.transparent : Colors.grey, // or any other color
+                                BlendMode.saturation,
+                              ),
+                              child: Image.network(
+                                data.photoUrl,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Icon(Icons
+                                      .image); // Placeholder icon if image fails to load
+                                },
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data.name,
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                        const SizedBox(height: 8),
+                        Text('Species: ${data.species}'),
+                        Text('Breed: ${data.breed}'),
+                        Text('Age: ${data.age}'),
+                        Text('Gender: ${data.gender}'),
+                        Text('Size: ${data.size}'),
+                        Text('Coat Color: ${data.coatColor}'),
+                        Text('Temperament: ${data.temperament}'),
+                        Text('Location: ${data.location}'),
+                      ],
+                    ),
+                  ),
+                  Row(
                     children: [
-                      Text(
-                        data.name,
-                        style: Theme.of(context).textTheme.displayMedium,
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ElevatedButton(
+                            onPressed:
+                                data.isAvailable && state is! DetailsLoadingState
+                                    ? () => _adoptPet(context, data)
+                                    : null,
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                data.isAvailable ? Colors.green : Colors.grey,
+                              ),
+                            ),
+                            child: const Text(Strings.adopt),
+                          ),
+                        ),
                       ),
-                      const SizedBox(height: 8),
-                      Text('Species: ${data.species}'),
-                      Text('Breed: ${data.breed}'),
-                      Text('Age: ${data.age}'),
-                      Text('Gender: ${data.gender}'),
-                      Text('Size: ${data.size}'),
-                      Text('Coat Color: ${data.coatColor}'),
-                      Text('Temperament: ${data.temperament}'),
-                      Text('Location: ${data.location}'),
                     ],
                   ),
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: ElevatedButton(
-                          onPressed:
-                              data.isAvailable && state is! DetailsLoadingState
-                                  ? () => _adoptPet(context, data)
-                                  : null,
-                          child: Text('Adopt'),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                              data.isAvailable ? Colors.green : Colors.grey,
-                            ),
-                          ),
-                        ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ConfettiWidget(
+                        blastDirectionality: BlastDirectionality.explosive,
+                        // colors: [Theme.of(context).primaryColor],
+                        blastDirection: 3 * pi / 2,
+                        numberOfParticles: 10,
+                        emissionFrequency: 0.4,
+                        gravity: 0.2,
+                        confettiController: confettiController,
+                        shouldLoop: false,
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ConfettiWidget(
-                      blastDirectionality: BlastDirectionality.explosive,
-                      // colors: [Theme.of(context).primaryColor],
-                      blastDirection: 3 * pi / 2,
-                      numberOfParticles: 10,
-                      emissionFrequency: 0.4,
-                      gravity: 0.2,
-                      confettiController: confettiController,
-                      shouldLoop: false,
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         });
