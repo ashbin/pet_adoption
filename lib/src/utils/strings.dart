98bcf48ddb30fca6925503879e923713847c95ac
ashbin <ashbinmathew22@gmail.com>
2023-07-14 15:18:41 +0530

    Update strings.dart

--- lib/src/utils/strings.dart
+++ lib/src/utils/strings.dart
@@ -2,6 +2,9 @@ class Strings{
   static const errorSomethingWrong = "Something went wrong.. Please try again";
   static const searchHint = 'Search with pet name, breed, or species';
 
+  static const petDetails = 'Pet Details';
+  static const adopt = 'Adopt';
+
   static const adoptionHistory = 'Adoption History';
   static const adoptionHistoryEmpty = "You haven't adopted any pet.. Go ahead and adopt today";
 }
